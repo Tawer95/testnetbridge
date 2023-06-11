8993e4c1c00a4fac165239e275f5911195055790
Tawer95 <73060710+Tawer95@users.noreply.github.com>
2023-06-11 22:41:37 +0500

    Update RPCs.py

--- bridge/RPCs.py
+++ bridge/RPCs.py
@@ -1,3 +1,3 @@
 ETH_RPC = 'https://eth.llamarpc.com'
-ARB_RPC = 'https://arb-mainnet.g.alchemy.com/v2/jAfYxbp_CBpbmnH_GAYc7sAMTay0BRmq'
-OPT_RPC = 'https://opt-mainnet.g.alchemy.com/v2/IN239nn0VgHAAXFAqBVNnritXkXm-EK2'
+ARB_RPC = ''
+OPT_RPC = ''
